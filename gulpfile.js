970deae18ea845950f21b78383128f93664cf4d4
Scott <scottdlivingstone@gmail.com>
2017-10-18 14:16:18 -0700

    add linting to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,40 @@
+// Load Gulp first
 var gulp = require('gulp');
+// Load Packages
+var uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    browserSync = require('browser-sync'),
+    eslint = require('gulp-eslint');
 
-gulp.task('default', function () {
-  // place code for your default task here
+gulp.task('scripts', ['lint'], function() {
+  gulp.src('./js/*.js')
+    .pipe(uglify())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(gulp.dest('./build/js'))
 });
+
+gulp.task('watch', function () {
+  // compile minified js files automatically
+  gulp.watch('js/*.js', ['scripts']);
+});
+
+gulp.task('browser-sync', function(){
+  // initialize local server from root directory
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+  // watch build files for change and reload the browser when a file is changed
+  gulp.watch(['*.html', 'build/css/*.css', 'build/js/*.js']).on('change', browserSync.reload);
+});
+
+gulp.task('lint', function(){
+  gulp.src(['./js/*.js', '!node_modules/**'])
+  .pipe(eslint())
+  .pipe(eslint.format())
+  .pipe(eslint.failAfterError());
+})
+
+
+gulp.task('default', ['scripts', 'browser-sync', 'watch'] );
--- js/script.js
+++ js/script.js
@@ -1,27 +1,26 @@
 (function($){
-  
-  var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
-  url += '?' + $.param({
-    'api-key': "e5a8f43f464d44158743411125fae91b"
-  });
-  $("#select-section").on('change', function(){
-    var section = $("#select-section").val();
-    $("#gallery").empty();
+
+  $('#select-section').on('change', function(){
+    var section = $('#select-section').val();
+    var url = 'https://api.nytimes.com/svc/topstories/v2/' + section + '.json';
+    url += '?' + $.param({
+      'api-key': 'e5a8f43f464d44158743411125fae91b'
+    });
+    $('#gallery').empty();
     $.ajax({
       url: url,
-      method: "GET",
+      method: 'GET',
     })
     .done(function(data) {
       console.log(data);
       $.each(data.results, function(i, value){
-        if (value.section.toLowerCase() == section){
-          var output = "<p>" + value.title + "</p>";
-          output += "<p>" + value.abstract + "</p>";
-          $("#gallery").append(output);
-        } 
+          var output = '<p>' + value.title + '</p>';
+          output += '<p>' + value.abstract + '</p>';
+          $('#gallery').append(output);
       });
     })
     .fail(function(){
+      var error = undefined;
       throw error;
     });
   });
