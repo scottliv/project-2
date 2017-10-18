fb4b34d518e6b6ba0aba43964c6429b0a4c01a6b
Scott <scottdlivingstone@gmail.com>
2017-10-18 16:41:31 -0700

    fix background images by creating unique classes for each appended item

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 (function($){
 
   $('#select-section').on('change', function(){
+
+    // construct the url based on user selection
     var section = $('#select-section').val();
     var url = 'https://api.nytimes.com/svc/topstories/v2/' + section + '.json';
     url += '?' + $.param({
       'api-key': 'e5a8f43f464d44158743411125fae91b'
     });
     $('#gallery').empty();
+    
+    // remove defaullt styling once a section is selected
+    if (section !== 'Section'){
+      $('header').removeClass('no-selection');
+    }
+
     $.ajax({
       url: url,
       method: 'GET',
@@ -15,18 +23,28 @@
       console.log(data);
       var articleCounter = 0;
       $.each(data.results, function(i, value){
-          if (articleCounter === 12){
+          if (i === 12){
             return false;
           }
           if(value.multimedia.length === 0){
             return true;
           }
-          var output = "<ol>";
-          output += '<li>' + value.title + '</li>';
-          output += "</ol>";
-          // output += '<p>' + value.abstract + '</p>';
+          var backgroundImage = value.multimedia[4].url;
+          // set a unique class on each item
+          var imageNumber = 'image-conatiner-'+i;
+          var output = '<li class="gallery-item"><a href="';
+          output += value.url;
+          output += '"><div class=' + imageNumber + '>';
+          output += value.abstract;
+          output += '</div></a></li>';
           $('#gallery').append(output);
-          articleCounter ++;
+          $('.' + imageNumber).css({
+            'background-image': 'url("' + backgroundImage + '")',
+            'background-size': 'cover',
+            'background-position': 'center',
+            'height': '50vw'
+          });
+          // articleCounter ++;
       });
     })
     .fail(function(){
