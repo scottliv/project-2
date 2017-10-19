93af3caf9dc1bbbd59a72be77690d5a44b1d2da8
Scott <scottdlivingstone@gmail.com>
2017-10-19 16:58:38 -0700

    Add style to form select

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 (function($){
 
   $('#select-section').on('change', function(){
-    // construct the url based on user selection
+    // Construct the url based on user selection
     var section = $('#select-section').val();
     var url = 'https://api.nytimes.com/svc/topstories/v2/' + section + '.json';
     url += '?' + $.param({
       'api-key': 'e5a8f43f464d44158743411125fae91b'
     });
-    // Clear the container
+    // Clear the container and add a loading .gif
     $('.gallery').children().remove();
     $('.gallery').append('<img class="loader" src="images/ajax-loader.gif"/>')
    
-    // remove defaullt styling once a section is selected
+    // Remove defaullt styling once a section is selected
     if (section !== 'section'){
       $('header').removeClass('no-selection');
       $('header').addClass('active');
@@ -26,8 +26,8 @@
       $('.logo').removeClass('active-logo')
       
     } 
-
     
+    // Retrieve data from NYT API
 
     $.ajax({
       url: url,
@@ -38,7 +38,7 @@
       console.log(data);
       var articleCounter = 0;
       $.each(data.results, function(i, value){
-        
+          // Check to see if image file exists, if there is no image, returning true comtinues to the next item in the loop
           if(value.multimedia.length === 0){
             return true;
           }
@@ -46,23 +46,27 @@
           var bestQuality = value.multimedia.length - 1;
           var backgroundImage = value.multimedia[bestQuality].url;
 
-          // set a unique class on each item for the background image
+          // set a unique class on each item for the background image + construct HTML 
           var imageNumber = 'image-conatiner-'+i;
           var output = '<li class="gallery-item"><a href="';
           output += value.url;
           output += '"><div class="image-container ' + imageNumber + '">';
           output += '<p class="abstract">' + value.abstract + '</p>';
           output += '</div></a></li>';
+          // Add elements to the DOM
           $('.gallery').append(output);
           // console.log(output);
+          //Selects image class and adds background image
           $('.' + imageNumber).css({
             'background-image': 'url("' + backgroundImage + '")',
             'background-size': 'cover',
             'background-position': 'center',
             'height': '100%'
           });
+          // Increment to article counter after an item has been appended
           articleCounter ++;
-          console.log(articleCounter);
+          // console.log(articleCounter);
+          // The loop will continue so long as the article counter does not equal 12
           return (articleCounter !== 12);
       });
     })
@@ -71,5 +75,6 @@
       return error;
     });
   });
-
+  // Selectric Form Styling
+  $('select').selectric();
 }(jQuery));
